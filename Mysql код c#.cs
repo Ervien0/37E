c347f2a177865d7d82c0d2230306829028ebc1a4
Ervien0 <131531783+Ervien0@users.noreply.github.com>
2025-06-29 18:53:20 +0300

    Create Mysql код c#.cs

---
+++ Mysql код c#.cs
@@ -0,0 +1,146 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Windows;
+using System.Windows.Controls;
+using MySql.Data.MySqlClient; //Нужно установить Mysql.Data через nuget (в поиске можно найти)
+
+namespace TicketApp
+{
+    public class Request
+    {
+        public int Id { get; set; }
+        public string ClientName { get; set; }
+        public string RequestType { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+
+    public partial class MainWindow : Window
+    {
+        private string connString = "Server=localhost;Database=service;Uid=root;Pwd=1234;";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadRequests();
+        }
+
+        private DbConnection CreateConn()
+        {
+            return new MySqlConnection(connString);
+        }
+
+        private void LoadRequests()
+        {
+            var list = new List<Request>();
+            using (var conn = CreateConn())
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT Id, ClientName, RequestType, Description, Status, CreatedAt FROM Requests ORDER BY CreatedAt DESC";
+                    using (var rdr = cmd.ExecuteReader())
+                    {
+                        while (rdr.Read())
+                        {
+                            list.Add(new Request
+                            {
+                                Id = rdr.GetInt32("Id"),
+                                ClientName = rdr.GetString("ClientName"),
+                                RequestType = rdr.GetString("RequestType"),
+                                Description = rdr.GetString("Description"),
+                                Status = rdr.GetString("Status"),
+                                CreatedAt = rdr.GetDateTime("CreatedAt")
+                            });
+                        }
+                    }
+                }
+            }
+            dgRequests.ItemsSource = list;
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            using (var conn = CreateConn())
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "INSERT INTO Requests (ClientName, RequestType, Description, Status) VALUES (@c,@t,@d,'новая')";
+
+                    var p1 = cmd.CreateParameter();
+                    p1.ParameterName = "@c";
+                    p1.Value = tbClient.Text;
+                    cmd.Parameters.Add(p1);
+
+                    var p2 = cmd.CreateParameter();
+                    p2.ParameterName = "@t";
+                    p2.Value = (cbType.SelectedItem as ComboBoxItem)?.Content.ToString();
+                    cmd.Parameters.Add(p2);
+
+                    var p3 = cmd.CreateParameter();
+                    p3.ParameterName = "@d";
+                    p3.Value = tbDesc.Text;
+                    cmd.Parameters.Add(p3);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            LoadRequests();
+        }
+
+        private void UpdateStatus_Click(object sender, RoutedEventArgs e)
+        {
+            if (dgRequests.SelectedItem is Request sel)
+            {
+                using (var conn = CreateConn())
+                {
+                    conn.Open();
+                    using (var cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = "UPDATE Requests SET Status=@st WHERE Id=@id";
+
+                        var p1 = cmd.CreateParameter();
+                        p1.ParameterName = "@st";
+                        p1.Value = (cbStatus.SelectedItem as ComboBoxItem)?.Content.ToString();
+                        cmd.Parameters.Add(p1);
+
+                        var p2 = cmd.CreateParameter();
+                        p2.ParameterName = "@id";
+                        p2.Value = sel.Id;
+                        cmd.Parameters.Add(p2);
+
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+                LoadRequests();
+            }
+        }
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            if (dgRequests.SelectedItem is Request sel)
+            {
+                using (var conn = CreateConn())
+                {
+                    conn.Open();
+                    using (var cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = "DELETE FROM Requests WHERE Id=@id";
+
+                        var p = cmd.CreateParameter();
+                        p.ParameterName = "@id";
+                        p.Value = sel.Id;
+                        cmd.Parameters.Add(p);
+
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+                LoadRequests();
+            }
+        }
+    }
+}
